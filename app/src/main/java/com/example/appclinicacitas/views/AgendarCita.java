2fc941fedf99533a1102311a33d261b8f1e4b807
elvisGutierrez22 <elvis.gutierrez@catolica.edu.sv>
2023-10-05 17:45:42 -0600

    Modificaciones de diseño

--- app/src/main/java/com/example/appclinicacitas/views/AgendarCita.java
+++ app/src/main/java/com/example/appclinicacitas/views/AgendarCita.java
@@ -2,7 +2,11 @@ package com.example.appclinicacitas.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.appclinicacitas.R;
 
@@ -12,5 +16,14 @@ public class AgendarCita extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar_cita);
+
+        Button botonIrAVistaPrincipal = findViewById(R.id.btnAgendarCita);
+        botonIrAVistaPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appclinicacitas/views/VerCitas.java
+++ app/src/main/java/com/example/appclinicacitas/views/VerCitas.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.appclinicacitas.R;
@@ -17,6 +18,14 @@ public class VerCitas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_citas);
 
+        ImageButton botonIrAVistaPrincipal = findViewById(R.id.btnSalirCitas);
+        botonIrAVistaPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
