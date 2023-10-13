22ade6d3b51719b62199125c886b5ef921adfa4c
manue <carlos.rivas1@catolica.edu.sv>
2023-10-12 20:46:51 -0600

    salir de sesion

--- app/src/main/java/com/example/appclinicacitas/views/MainActivity.java
+++ app/src/main/java/com/example/appclinicacitas/views/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.appclinicacitas.views;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.appclinicacitas.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button botonIrAVistaLogin;
 
 
     @Override
@@ -35,13 +41,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button botonIrAVistaLogin = findViewById(R.id.btnSalirMenu);
-        botonIrAVistaLogin.setOnClickListener(new View.OnClickListener() {
+        botonIrAVistaLogin = findViewById(R.id.btnSalirMenu);
+        botonIrAVistaLogin.setOnClickListener((v)->showMenu());
+
+
+
+    }
+    void showMenu() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Confirmar Logout");
+        builder.setMessage("¿Está seguro de que desea cerrar sesión?");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth.getInstance().signOut();
+
                 Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
+                finish(); // Cierra la actividad actual
             }
         });
-    }
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // El usuario seleccionó "No", no se hace nada
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+}
 }
